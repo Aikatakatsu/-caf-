da3bcd4d9d8f3e3640f57e3f779095542f40f6b4
kenyaonuma <m212@skytree2021.site>
2021-06-15 09:31:07 +0900

    Revert "性別ごとの円グラフ表示"
    This reverts commit 98723eb5923cb7841f833fd013bf535ce870cb44, reversing
    changes made to 3a874e97ceccf02188a3210acce21a6a2aa4b34f.

--- mysite/analysis/urls.py
+++ mysite/analysis/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+# views.pyとurls.pyは同じディレクトリに存在しているため「from . 」（カレントディレクトリからImportする
 from . import views
 
-# from django.contrib.auth.models import User
+app_name = 'analysis'  # analysisという名前空間の中にあるものを，htmlから呼び出しがあると，それに対応したviews.pyを引き出す
 
-# analysisという名前空間を設定
-app_name = 'analysis'
+# 全体のURLパスは
+# 親のurls.pyの定義 |    アプリケーションのurls.pyの定義  になる
+# 一番上はhttp://127.0.0.1/analysis/analysis_list/になる
 
 urlpatterns = [
 	path('top/', views.topview, name='topview'),
@@ -15,26 +17,5 @@ urlpatterns = [
 	path('update_done/', views.update_done, name='update_done'),
 	path('delete/<int:pk>/', views.AnalysisDeleteView.as_view(), name='analysis_delete'),
 	path('delete_done/', views.delete_done, name='delete_done'),
-	# ログイン画面
-	path('top/', views.topview, name='topview'),
-	# 管理者用の一覧画面
-	path('analysis_list/', views.AnalysisListView.as_view(), name='analysis_list'),
-	# 新規登録：ユーザー名，パスワード等画面
-	path('register_create/', views.AnalysisRegisterView.as_view(), name='register_create'),
-	# ユーザ登録完了画面
-	path('register_done/', views.register_done, name='register_done'),
-	# 新規登録：個人情報を登録する画面
-	path('analysis_create/<int:pk>/', views.AnalysisCreateView.as_view(), name='analysis_create'),
-	# 新規登録完了画面
-	path('create_done/', views.create_done, name='create_done'),
-	# 更新画面
-	path('update/<int:pk>/', views.AnalysisUpdateView.as_view(), name='analysis_update'),
-	# 更新完了画面
-	path('update_done/', views.update_done, name='update_done'),
-	# 削除画面
-	path('delete/<int:pk>/', views.AnalysisDeleteView.as_view(), name='analysis_delete'),
-	# 削除完了画面
-	path('delete_done/', views.delete_done, name='delete_done'),
-	# 円グラフ表示
 	path('circle/', views.show_gender_circle_graph, name='gender_circle'),
 ]
